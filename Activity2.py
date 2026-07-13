bcb6e961858d9c6650be2ea6fef30f44f06cb890
unknown <maheksolanki140314@gmail.com>
2026-07-13 12:27:29 +0400

    activity2 done

---
+++ Activity2.py
@@ -0,0 +1,14 @@
+# print a sentence
+print("Welcome to the world of programming")
+
+# print a number
+print(12)
+
+# next line after print
+print("hello python\n")
+
+# pass multiple arguments for printing 
+print("Hello", 34, "\nby Mahek")
+
+# end argument of print statement
+print("Welcome to ", end="*")
