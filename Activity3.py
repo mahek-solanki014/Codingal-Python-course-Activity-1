30b8cc78b9b5bea7005765fc510b70c0132738fc
unknown <maheksolanki140314@gmail.com>
2026-07-13 12:44:32 +0400

    activity3 done

---
+++ Activity3.py
@@ -0,0 +1,14 @@
+# creating different variables
+x=8
+y="Mahek"
+
+print(x)
+print(y)
+
+codingal="assignment codingal"
+print(codingal)
+
+# user input
+Name=input("Enter you name: ")
+
+print("\n Hello", Name, "\n Welcome to codingal")
